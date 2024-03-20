77ab2a10a6644d8b2688415890eb221d5bf05c7a
LobinhoTroll <gabrielteles931@gmail.com>
2024-03-20 14:30:42 -0300

    Adicionando o inicio da atividade da semana 2

---
+++ src/Cepedi.WebApi/Controllers/CursoController.cs
@@ -0,0 +1,26 @@
+﻿using Cepedi.IoC;
+using Cepedi.IoC.services;
+using Cepedi.Shareable.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cepedi.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CursoController : ControllerBase
+{
+    private readonly ILogger<CursoController> _logger;
+    private readonly CursoServiceCollection _service;
+
+    public CursoController(ILogger<CursoController> logger, CursoServiceCollection service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+
+    [HttpGet("{idCurso}")]
+    public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
+    {
+        return Ok(_service.GetByID(idCurso));
+    }
+}
