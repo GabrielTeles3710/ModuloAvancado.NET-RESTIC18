780d98202ea4855887c3c84c9c6b9e74884f7506
LobinhoTroll <gabrielteles931@gmail.com>
2024-04-24 23:58:09 -0300

    adicionando o momento  onde a mensagem seria enviada

--- Exercício-5/Cepedi.Banco.Analise/src/Cepedi.Banco.Analise.Dominio/Handlers/AtualizarUsuarioRequestHandller.cs
+++ Exercício-5/Cepedi.Banco.Analise/src/Cepedi.Banco.Analise.Dominio/Handlers/AtualizarUsuarioRequestHandller.cs
@@ -1,53 +1,53 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
-// using Cepedi.Banco.Analise.Dominio.Repository;
-// using Cepedi.Banco.Analise.Compartilhado.Requests;
-// using Cepedi.Banco.Analise.Compartilhado.Responses;
-// using Cepedi.Banco.Analise.Compartilhado.Services;
-// using MediatR;
-// using Microsoft.Extensions.Logging;
-// using OperationResult;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cepedi.Banco.Analise.Dominio.Repository;
+using Cepedi.Banco.Analise.Compartilhado.Requests;
+using Cepedi.Banco.Analise.Compartilhado.Responses;
+using Cepedi.Banco.Analise.Compartilhado.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
 
-// namespace Cepedi.Banco.Analise.Dominio.Handlers;
-// public class AtualizarUsuarioRequestHandler :
-//     IRequestHandler<AtualizarUsuarioRequest, Result<AtualizarUsuarioResponse>>
-// {
-//     private readonly IUsuarioRepository _usuarioRepository;
-//     private readonly ILogger<AtualizarUsuarioRequestHandler> _logger;
+namespace Cepedi.Banco.Analise.Dominio.Handlers;
+public class AtualizarUsuarioRequestHandler :
+    IRequestHandler<AtualizarUsuarioRequest, Result<AtualizarUsuarioResponse>>
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly ILogger<AtualizarUsuarioRequestHandler> _logger;
 
-//     public AtualizarUsuarioRequestHandler(IUsuarioRepository usuarioRepository, ILogger<AtualizarUsuarioRequestHandler> logger)
-//     {
-//         _usuarioRepository = usuarioRepository;
-//         _logger = logger;
-//     }
+    public AtualizarUsuarioRequestHandler(IUsuarioRepository usuarioRepository, ILogger<AtualizarUsuarioRequestHandler> logger)
+    {
+        _usuarioRepository = usuarioRepository;
+        _logger = logger;
+    }
 
-//     public async Task<Result<AtualizarUsuarioResponse>> Handle(AtualizarUsuarioRequest request, CancellationToken cancellationToken)
-//     {
-//         try
-//         {
-//             var usuarioEntity = await _usuarioRepository.ObterUsuarioAsync(request.Id);
+    public async Task<Result<AtualizarUsuarioResponse>> Handle(AtualizarUsuarioRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var usuarioEntity = await _usuarioRepository.ObterUsuarioAsync(request.Id);
 
-//             if (usuarioEntity == null)
-//             {
-//                 return Result.Error<AtualizarUsuarioResponse>(new Shareable.
-//                     Exceptions.SemResultadosException());
-//             }
+            if (usuarioEntity == null)
+            {
+                return Result.Error<AtualizarUsuarioResponse>(new Shareable.
+                    Exceptions.SemResultadosException());
+            }
 
-//             usuarioEntity.Atualizar(request.Nome);
+            usuarioEntity.Atualizar(request.Nome);
 
-//             await _usuarioRepository.AtualizarUsuarioAsync(usuarioEntity);
+            await _usuarioRepository.AtualizarUsuarioAsync(usuarioEntity);
             
-//             RabbitMQProducer.EnviarMenssagem();
+            RabbitMQProducer.EnviarMensagem();
 
-//             return Result.Success(new AtualizarUsuarioResponse(usuarioEntity.Nome));
-//         }
-//         catch
-//         {
-//             _logger.LogError("Ocorreu um erro ao atualizar os usuários");
-//             throw;
-//         }
-//     }
-// }
+            return Result.Success(new AtualizarUsuarioResponse(usuarioEntity.Nome));
+        }
+        catch
+        {
+            _logger.LogError("Ocorreu um erro ao atualizar os usuários");
+            throw;
+        }
+    }
+}
